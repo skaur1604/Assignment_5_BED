6101bce617b5f91caabd36f67e20feea7e3ca030
skaur1604 <skaur1604@academic.rrc.ca>
2025-10-16 22:11:08 -0500

    Create branchValidation.ts file in Validation folder and work in it

---
+++ src/api/v1/Validation/branchValidation.ts
@@ -0,0 +1,10 @@
+import Joi from "joi";
+
+export const branchCreateSchema = Joi.object({
+  name: Joi.string().trim().min(3).max(100).required(),
+  address: Joi.string().trim().min(10).max(200).required(),
+  phone: Joi.string()
+    .trim()
+    .pattern(/^\+?[0-9\s\-()]{8,25}$/)
+    .required(),
+});
