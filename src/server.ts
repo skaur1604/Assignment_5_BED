fd2a6a4923f9e58ff1acf392a99f544637e3ad5f
skaur1604 <skaur1604@academic.rrc.ca>
2025-11-18 20:40:35 -0600

    feat: secure sensitive config using dotenv and env variables

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import app from "./app";
 
-const PORT = 3000;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+const port = process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
 });
